224e33144e6bbb1d27d8ba3c9aae9cfe1e79bd80
Ryan Filgas <rfilgas@pdx.edu>
2022-08-01 14:27:29 -0700

    Added navbar and site skeleton

--- home.js
+++ home.js
@@ -1,1 +1,13 @@
-
+// let navbar = document.getElementById('navbar');
+// let navbar_container = document.createElement("div");
+// let navtext = document.createElement("h1");
+// navtext.textContent = " Intro Previous Work Projects Contact"
+// navbar_container.appendChild(navtext);
+// navbar.appendChild(navbar_container);
+//   "Would you like to sign up for the newsletter?";
+// newsletter_question.class = "newsletter_question";
+// newsletter_question.style.fontWeight = "bold";
+// newsletter_question.style.padding = "0";
+// newsletter_question.style.margin = "0";
+// form.appendChild(question_container);
+// question_container.appendChild(newsletter_question);
